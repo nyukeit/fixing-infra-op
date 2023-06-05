c3cd683bbe27ae4f742865765214e72b03e82a65
Nyukeit <nyukeit@outlook.com>
2023-06-05 10:21:22 +0200

    [Modify] Variables file cleanup

--- tf-modules/variables.tf
+++ tf-modules/variables.tf
@@ -1,51 +1,31 @@
 variable "ami_id" {
-  default = "ami-08fdec01f5df9998f" 
+  default = "ami-053b0d53c279acc90" 
 }
 
-variable "vpc_id"{
-  default= "vpc-0628a7cccdfec986d"
-}
+#variable "vpc_id"{
+#  default= "vpc-0628a7cccdfec986d"
+#}
 
 variable "key_name" {
   default = "infra_op"
 }
 
 variable "aws_access_key" {
-  default = "ASIAZWKLZ6WUXXLT5ZOP"
+  default = "ASIAZWKLZ6WUYSBXMQGK"
 }
 
 variable "aws_secret_key" {
-  default = "4prAIytje5wi2epvwVn/reM2PBFYiHkQchcbEQ6b"
+  default = "zKRl72QO3pWf0U+/djYEmCLDKqVfoWeR6iR3QeZH"
 }
 
 variable "aws_token" {
-  default = "FwoGZXIvYXdzENn//////////wEaDDUAAOXQi0CEFLL8qSK0AdNjuMhUs65MXNMFUf23VHuwgI0u7sgmfq+RbsaYhWUE5MBb9KfqR0hMo6MKVhAJ/Tc/mYEJsTWHx+zOSTHCzXSIAoPLM/pxB04k0abAwCXRYWaWRB0/Msi+3t8LTlThwS+fRF7Cs3acsLkLHcKuqNHBVDwcwxbc5ceFkeoYmhghEO2xHCfL7HBIi7LLb3FyxQSAAI7damdduf27ySu8ZMvCdiL86YYd8w9nf5Njg7pc4pngkCisg/GjBjItyFY8UyXhw2BqbTnCrkCRPea+/ggTheGY5joMR2SiJNVn85Qng4ds7Qt+jZWu"
+  default = "FwoGZXIvYXdzEPL//////////wEaDHkd+XlrVVfWePUUMSK0AYkI09LzbszeGuR+ZY1fswfv67nr7LojtFnZLiNTvlIar69WMJXOhprHxCIOvlWJZZmCugrADecInQDsHwlW+BGWdTa/un4dupZyGS096Av+DHdhbms3h9ypZA5UdAZjCsQVEM0nwuaIQ6eT77cn5LTvi4JFqg6xOryFyy2mbadDBX4csYOFqGRioP9zG2Z37T0tPQS0UpUAaMFghxAiQ9UoLsKtg6MrVT1UrRkLDdAGSf9qvSj0sfajBjItenz/7JTBMi0nnie73A4gAMpXPP5BPcxMo2btVT7SY+WvFMsKgtisnGBEAr0n"
 }
 
 variable "region" {
   default = "us-east-1"
 }
 
-variable "region_2" {
-  default = "us-east-2"
-}
-
 variable "ssh_user" {
   default = "ubuntu"
-}
-
-variable "subnet_id_1" {
-  default = "subnet-0dbbe5280c7146547"
-}
-
-variable "subnet_id_2" {
-  default = "subnet-0ba91d2ba87da6c7f"
-}
-
-variable "subnet_id_3" {
-  default = "subnet-070c49941cb44596f"
-}
-
-variable "igw_id" {
-  default = "igw-0c23c43f25dcccd8c"
 }
